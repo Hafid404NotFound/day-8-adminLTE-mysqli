db977381d64ba760381cf5da153046b1d5880e2d
hafid1994 <hafidz25.ma@gmail.com>
2024-09-19 06:28:47 +0700

    first commit

---
+++ pages/tables/controler.php
@@ -0,0 +1,12 @@
+<?php 
+
+$db = mysqli_connect('localhost','root', '','customer-agustus');
+
+
+
+// check koneksi
+// if ( $db) {
+//   echo "Berhasill yuhu";
+// }else {
+//   echo "GAGALLLL";
+// }
